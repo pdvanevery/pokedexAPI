e8676e2a23df60dcbb9c357bf70d521847186cc7
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-05-03 19:58:25 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a react app that lets you search up a Pokemon and basic information about them.
+To get started, type in a pokemon's name in the searchbar, then click "Search Pokemon"
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css'
+import { useState } from 'react'
+import Axios from 'axios'
+
+function App() {
+  const [pokemonName, setPokemonName] = useState('')
+  const [pokemonChosen, setPokemonChosen] = useState(false)
+  const [pokemon, setPokemon] = useState({
+    name: '',
+    species: '',
+    img: '',
+    hp: '',
+    attack: '',
+    defense: '',
+    special_attack: '',
+    special_defense: '',
+    speed: '',
+    type: '',
+    type2: '',
+  })
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+      setPokemon({
+        name: pokemonName,
+        species: response.data.species.name,
+        img: response.data.sprites.front_default,
+        img2: response.data.sprites.front_shiny, 
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        special_attack: response.data.stats[3].base_stat,
+        special_defense: response.data.stats[4].base_stat,
+        speed: response.data.stats[5].base_stat,
+        type: response.data.types[0].type.name,
+      })
+      setPokemonChosen(true)
+    })
+  }
+
+  
+  return (
+    <div className='App'>
+      <div className='Title'>
+        <h1>Pokemon Stats</h1>
+        <input type='text' onChange={(event) => {setPokemonName(event.target.value)}} />
+        <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+      <div className='Display'>
+        {!pokemonChosen ? (
+        <h1> Please choose a Pokemon</h1>) : (
+        <>
+        <h1>{pokemon.species}</h1>
+        <div className='PokemonImage'>
+        <img src={pokemon.img} />
+        <img src={pokemon.img2} />
+        </div>
+          <h3>Type: {pokemon.type}</h3>
+          <h4>HP: {pokemon.hp}</h4>
+          <h4>Attack: {pokemon.attack}</h4>
+          <h4>Defense: {pokemon.defense}</h4>
+          <h4>Special Attack: {pokemon.special_attack}</h4>
+          <h4>Special Defense: {pokemon.special_defense}</h4>
+          <h4>Speed: {pokemon.speed}</h4>
+        </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
